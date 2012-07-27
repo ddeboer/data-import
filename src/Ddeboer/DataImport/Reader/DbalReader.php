5c48c13447958de21b1486f35114c4f73db717a5
David de Boer <david@ddeboer.nl>
2012-07-27 11:02:43 +0200

    Clean up

--- src/Ddeboer/DataImport/Reader/DbalReader.php
+++ src/Ddeboer/DataImport/Reader/DbalReader.php
@@ -3,6 +3,9 @@ namespace Ddeboer\DataImport\Reader;
 
 use Doctrine\DBAL\Connection;
 
+/**
+ * Reads data through the Doctrine DBAL
+ */
 class DbalReader implements ReaderInterface
 {
     protected $connection;
@@ -10,12 +13,17 @@ class DbalReader implements ReaderInterface
     protected $table;
     protected $data;
 
+    /**
+     * Constructor
+     *
+     * @param Connection $connection Database connection
+     * @param string     $table      Database table name
+     */
     public function __construct(Connection $connection, $table)
     {
         $this->connection = $connection;
         $this->schemaManager = $connection->getSchemaManager();
         $this->table = $table;
-        $this->loadData();
     }
 
     protected function loadData()
@@ -23,6 +31,9 @@ class DbalReader implements ReaderInterface
         $this->data = $this->connection->fetchAll('SELECT * FROM ' . $this->table);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getFields()
     {
         $fields = array();
@@ -34,27 +45,43 @@ class DbalReader implements ReaderInterface
         return $fields;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function current()
     {
         return current($this->data);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function next()
     {
         next($this->data);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function key()
     {
         return key($this->data);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function valid()
     {
         $key = key($this->data);
+
         return ($key !== null && $key !== false);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function rewind()
     {
         reset($this->data);
