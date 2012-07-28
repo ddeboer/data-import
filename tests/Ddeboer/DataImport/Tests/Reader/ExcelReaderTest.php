91e9bb939b95a53700f5ad4eda074b2ea18fd6b6
David de Boer <david@ddeboer.nl>
2012-07-28 12:37:01 +0200

    Improve tests

--- tests/Ddeboer/DataImport/Tests/Reader/ExcelReaderTest.php
+++ tests/Ddeboer/DataImport/Tests/Reader/ExcelReaderTest.php
@@ -6,6 +6,13 @@ use Ddeboer\DataImport\Reader\ExcelReader;
 
 class ExcelReaderTest extends \PHPUnit_Framework_TestCase
 {
+    public function testGetFields()
+    {
+        $file = new \SplFileObject(__DIR__.'/../Fixtures/data_column_headers.xlsx');
+        $reader = new ExcelReader($file, 0);
+        $this->assertEquals(array('id', 'number', 'description'), $reader->getFields());
+    }
+
     public function testCountWithoutHeaders()
     {
         $file = new \SplFileObject(__DIR__.'/../Fixtures/data_no_column_headers.xls');
@@ -16,8 +23,17 @@ class ExcelReaderTest extends \PHPUnit_Framework_TestCase
     public function testCountWithHeaders()
     {
         $file = new \SplFileObject(__DIR__.'/../Fixtures/data_column_headers.xlsx');
-        $reader = new ExcelReader($file);
-        $reader->setHeaderRowNumber(0);
+        $reader = new ExcelReader($file, 0);
         $this->assertEquals(3, $reader->count());
+    }
+
+    public function testIterate()
+    {
+        $file = new \SplFileObject(__DIR__.'/../Fixtures/data_column_headers.xlsx');
+        $reader = new ExcelReader($file, 0);
+        foreach ($reader as $row) {
+            $this->assertInternalType('array', $row);
+            $this->assertEquals(array('id', 'number', 'description'), array_keys($row));
+        }
     }
 }
