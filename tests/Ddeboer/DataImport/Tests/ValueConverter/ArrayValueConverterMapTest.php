7fb4b9e493b87e444df430cb851b86e575ad7615
crosse <crosse@2bepublished.at>
2013-07-08 10:53:13 +0200

    Fix whitespaces.

--- tests/Ddeboer/DataImport/Tests/ValueConverter/ArrayValueConverterMapTest.php
+++ tests/Ddeboer/DataImport/Tests/ValueConverter/ArrayValueConverterMapTest.php
@@ -10,44 +10,44 @@ use Ddeboer\DataImport\ValueConverter\CallbackValueConverter;
  */
 class ArrayValueConverterMapTest extends \PHPUnit_Framework_TestCase
 {
-	/**
-	 * @expectedException InvalidArgumentException
-	 */
-	public function testConvertWithNoArrayArgument()
-	{
-		$converter = new ArrayValueConverterMap(array('foo' => new CallbackValueConverter(function($input) {return $input;})));
-		$converter->convert('foo');
-	}
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testConvertWithNoArrayArgument()
+    {
+        $converter = new ArrayValueConverterMap(array('foo' => new CallbackValueConverter(function($input) {return $input;})));
+        $converter->convert('foo');
+    }
 
-	public function testConvertWithMultipleFields()
-	{
-		$data = array(
-			array(
-				'foo' => 'test',
-				'bar' => 'test'
-			),
-			array(
-				'foo' => 'test2',
-				'bar' => 'test2'
-			),
-		);
+    public function testConvertWithMultipleFields()
+    {
+        $data = array(
+            array(
+                'foo' => 'test',
+                'bar' => 'test'
+            ),
+            array(
+                'foo' => 'test2',
+                'bar' => 'test2'
+            ),
+        );
 
-		$addBarConverter = new CallbackValueConverter(function($input) { return 'foo'.$input; });
-		$addBazConverter = new CallbackValueConverter(function($input) { return 'baz'.$input; });
+        $addBarConverter = new CallbackValueConverter(function($input) { return 'foo'.$input; });
+        $addBazConverter = new CallbackValueConverter(function($input) { return 'baz'.$input; });
 
-		$converter = new ArrayValueConverterMap(
-			array(
-				'foo' => array($addBarConverter),
-				'bar' => array($addBazConverter, $addBarConverter),
-			)
-		);
+        $converter = new ArrayValueConverterMap(
+            array(
+                'foo' => array($addBarConverter),
+                'bar' => array($addBazConverter, $addBarConverter),
+            )
+        );
 
-		$data = $converter->convert($data);
+        $data = $converter->convert($data);
 
-		$this->assertEquals('footest', $data[0]['foo']);
-		$this->assertEquals('foobaztest', $data[0]['bar']);
+        $this->assertEquals('footest', $data[0]['foo']);
+        $this->assertEquals('foobaztest', $data[0]['bar']);
 
-		$this->assertEquals('footest2', $data[1]['foo']);
-		$this->assertEquals('foobaztest2', $data[1]['bar']);
-	}
+        $this->assertEquals('footest2', $data[1]['foo']);
+        $this->assertEquals('foobaztest2', $data[1]['bar']);
+    }
 }
