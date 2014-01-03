017fc0df98aac169396795588d1d7720e59289ba
David de Boer <david@ddeboer.nl>
2014-01-03 21:54:12 +0100

    Simplify ArrayReader

--- src/Ddeboer/DataImport/Reader/ArrayReader.php
+++ src/Ddeboer/DataImport/Reader/ArrayReader.php
@@ -7,34 +7,8 @@ namespace Ddeboer\DataImport\Reader;
  *
  * @author David de Boer <david@ddeboer.nl>
  */
-class ArrayReader implements ReaderInterface
+class ArrayReader extends \ArrayIterator implements ReaderInterface
 {
-    /**
-     * Construt array reader
-     *
-     * @param array $data Data array
-     */
-    public function __construct(array $data)
-    {
-        $this->data = $data;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function count()
-    {
-        return count($this->data);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function current()
-    {
-        return current($this->data);
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -42,39 +16,9 @@ class ArrayReader implements ReaderInterface
     {
         // Examine first row
         if ($this->count() > 0) {
-            return array_keys($this->data[0]);
+            return \array_keys($this[0]);
         }
-    }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function key()
-    {
-        return key($this->data);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function next()
-    {
-        return next($this->data);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function rewind()
-    {
-        reset($this->data);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function valid()
-    {
-        return isset($this->data[$this->key()]);
+        return array();
     }
 }
