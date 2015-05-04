d27a581f4f65572e3c672c22a15c471065b6a266
Igor Mukhin <igor.mukhin@gmail.com>
2015-05-05 00:39:54 +0300

    CsvWriter: Added option to prepend header row with column names

--- src/Writer/CsvWriter.php
+++ src/Writer/CsvWriter.php
@@ -23,6 +23,12 @@ class CsvWriter extends AbstractStreamWriter
      * @var boolean
      */
     private $utf8Encoding = false;
+    private $row = 1;
+
+    /**
+     * @var boolean
+     */
+    protected $prependHeaderRow;
 
     /**
      * @param string   $delimiter The delimiter
@@ -30,13 +36,15 @@ class CsvWriter extends AbstractStreamWriter
      * @param resource $stream
      * @param boolean  $utf8Encoding
      */
-    public function __construct($delimiter = ';', $enclosure = '"', $stream = null, $utf8Encoding = false)
+    public function __construct($delimiter = ';', $enclosure = '"', $stream = null, $utf8Encoding = false, $prependHeaderRow = false)
     {
         parent::__construct($stream);
 
         $this->delimiter = $delimiter;
         $this->enclosure = $enclosure;
         $this->utf8Encoding = $utf8Encoding;
+
+        $this->prependHeaderRow = $prependHeaderRow;
     }
 
     /**
@@ -54,6 +62,11 @@ class CsvWriter extends AbstractStreamWriter
      */
     public function writeItem(array $item)
     {
+        if ($this->prependHeaderRow && 1 == $this->row++) {
+            $headers = array_keys($item);
+            fputcsv($this->getStream(), $headers, $this->delimiter, $this->enclosure);
+        }
+
         fputcsv($this->getStream(), $item, $this->delimiter, $this->enclosure);
     }
 }
