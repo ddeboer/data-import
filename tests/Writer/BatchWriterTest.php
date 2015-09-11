ef130aeae1a544eebca4dcab5cbab2fa34e82b1e
Steve Buzonas <sbuzonas@carnegielearning.com>
2015-09-11 03:02:29 -0400

    update test cases to reflect correct flushing and preparation in batch writer

--- tests/Writer/BatchWriterTest.php
+++ tests/Writer/BatchWriterTest.php
@@ -26,12 +26,38 @@ class BatchWriterTest extends \PHPUnit_Framework_TestCase
         $delegate = $this->getMock('Ddeboer\DataImport\Writer');
         $writer = new BatchWriter($delegate);
 
-        $delegate->expects($this->exactly(20))
+        $delegate->expects($this->exactly(40))
             ->method('writeItem');
+
+        $delegate->expects($this->once())
+            ->method('prepare');
 
         $writer->prepare();
 
-        for ($i = 0; $i < 20; $i++) {
+        for ($i = 0; $i < 40; $i++) {
+            $writer->writeItem(['Test']);
+        }
+    }
+
+    public function testFlushWithFlushableDelegate()
+    {
+        $prophet = new \Prophecy\Prophet;
+        $prophecy = $prophet->prophesize();
+
+        $prophecy->willExtend('stdClass');
+        $prophecy->willImplement('Ddeboer\DataImport\Writer');
+        $prophecy->willImplement('Ddeboer\DataImport\Writer\FlushableWriter');
+
+        $prophecy->writeItem(\Prophecy\Argument::any())->shouldBeCalledTimes(40);
+
+        $prophecy->prepare()->shouldBeCalledTimes(2);
+        $prophecy->flush()->shouldBeCalled();
+
+        $writer = new BatchWriter($prophecy->reveal());
+
+        $writer->prepare();
+
+        for ($i = 0; $i < 40; $i++) {
             $writer->writeItem(['Test']);
         }
     }
