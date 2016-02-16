82f01fbb4cb2e65c40c34366065e48eb01d0f69c
Joel <joel@MacBook-Pro-de-Joel.local>
2016-02-16 15:29:04 +0100

    Replace deprecated call to make validator work with symfony >2.7

--- src/Filter/ValidatorFilter.php
+++ src/Filter/ValidatorFilter.php
@@ -2,7 +2,7 @@
 
 namespace Ddeboer\DataImport\Filter;
 
-use Symfony\Component\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints;
 use Ddeboer\DataImport\Exception\ValidationException;
