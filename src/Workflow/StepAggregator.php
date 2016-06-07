c2939280d98e6e3d2af65368b9ea44a85c4ed4a3
Matt Rosenquist <mattrq@gmail.com>
2016-06-07 10:51:02 +0100

    Update StepAggregator.php

--- src/Workflow/StepAggregator.php
+++ src/Workflow/StepAggregator.php
@@ -80,9 +80,7 @@ class StepAggregator implements Workflow, LoggerAwareInterface
      */
     public function addStep(Step $step, $priority = null)
     {
-        if (null === $priority) {
-            $priority =  $step instanceof PriorityStep ? $step->getPriority() : null;
-        }
+        $priority = null === $priority && $step instanceof PriorityStep ? $step->getPriority() : $priority;
         $priority = null === $priority ? 0 : $priority;
 
         $this->steps->insert($step, $priority);
