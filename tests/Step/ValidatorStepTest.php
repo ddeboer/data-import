293cf4764f99acd1dedc3f4dd24aa076e70627d2
ipetrovic <ivana.petrovic@trivago.com>
2016-11-08 11:56:21 +0100

    Adding unit test for ValidatorStep new method allowExtraFields and adjusting phpunit dependencies in composer

--- tests/Step/ValidatorStepTest.php
+++ tests/Step/ValidatorStepTest.php
@@ -4,15 +4,20 @@ namespace Ddeboer\DataImport\Tests\Step;
 
 use Ddeboer\DataImport\Step\ValidatorStep;
 use Symfony\Component\Validator\Constraints;
-use Symfony\Component\Validator\ConstraintViolation;
 use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ValidatorStepTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var ValidatorInterface */
+    private $validator;
+
+    /** @var ValidatorStep */
+    private $filter;
+
     protected function setUp()
     {
-        $this->validator = $this->getMock('Symfony\Component\Validator\Validator\ValidatorInterface');
-
+        $this->validator = $this->createMock(ValidatorInterface::class);
         $this->filter = new ValidatorStep($this->validator);
     }
 
@@ -35,7 +40,7 @@ class ValidatorStepTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException Ddeboer\DataImport\Exception\ValidationException
+     * @expectedException \Ddeboer\DataImport\Exception\ValidationException
      */
     public function testProcessWithExceptions()
     {
@@ -54,6 +59,26 @@ class ValidatorStepTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($this->filter->process($data));
     }
 
+    public function testProcessWithAllowedExtraFields()
+    {
+        $this->filter->allowExtraFields();
+
+        $data = ['title' => null, 'telephone' => '0155/555-555'];
+
+        $this->filter->add('title', $constraint = new Constraints\NotNull());
+
+        $list = new ConstraintViolationList();
+        $list->add($this->buildConstraintViolation());
+
+        $this->validator->expects($this->once())
+            ->method('validate')
+            ->willReturn($list);
+
+        $this->assertFalse($this->filter->process($data));
+
+        $this->assertEquals([1 => $list], $this->filter->getViolations());
+    }
+
     public function testPriority()
     {
         $this->assertEquals(128, $this->filter->getPriority());
@@ -61,8 +86,6 @@ class ValidatorStepTest extends \PHPUnit_Framework_TestCase
 
     private function buildConstraintViolation()
     {
-        return $this->getMockBuilder('Symfony\Component\Validator\ConstraintViolation')
-                    ->disableOriginalConstructor()
-                    ->getMock();
+        return $this->createMock('Symfony\Component\Validator\ConstraintViolation');
     }
 }
